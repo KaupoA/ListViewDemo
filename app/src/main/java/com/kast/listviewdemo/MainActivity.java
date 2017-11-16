f3bdc3a2de3a4784cb75bd5136e6dfb457603b38
Kaupo Aun <kaupo.aun@gmail.com>
2017-11-16 21:09:03 +0200

    Finished demo

--- app/src/main/java/com/kast/listviewdemo/MainActivity.java
+++ app/src/main/java/com/kast/listviewdemo/MainActivity.java
@@ -2,8 +2,12 @@ package com.kast.listviewdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -14,16 +18,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView listView = (ListView) findViewById(R.id.listView);
+        ListView friendsList = (ListView)findViewById(R.id.friendsList);
 
-        ArrayList<String> familyMembers = new ArrayList<String>();
+        final ArrayList<String> myFriends = new ArrayList<>();
 
-        familyMembers.add("Birgit", "Kaupo", "Merika", "Kuno");
-     //   familyMembers.add("Kaupo");
-     //   familyMembers.add("Merika");
-     //   familyMembers.add("Kuno");
+        myFriends.add("Mikk");
+        myFriends.add("Aleks");
+        myFriends.add("Laura");
+        myFriends.add("Siim");
+        myFriends.add("Knut");
+        myFriends.add("Nils");
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>();
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myFriends);
+
+        friendsList.setAdapter(arrayAdapter);
+
+        friendsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(MainActivity.this, "Hello, " + myFriends.get(position) + "!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
     }
 }
